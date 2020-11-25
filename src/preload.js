21069abcfa88a1fb9c7d3df1630f4d58fa10b41c
unknown <myadiuio@gmail.com>
2020-11-25 20:59:51 +0900

    Moved files

---
+++ src/preload.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const url = require('url');
+
+const customTitlebar = require('custom-electron-titlebar');
+
+window.addEventListener('DOMContentLoaded', () => {
+    new customTitlebar.Titlebar({
+        backgroundColor: customTitlebar.Color.fromHex('#323232'),
+        icon: url.format(path.join(__dirname, '/img/icon.png')),
+        maximizable: false,
+        menu: null,
+        titleHorizontalAlignment: "left"
+    });
+
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+    }
+})
